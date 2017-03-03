b6951a892305eb604419917acbe5dd5c0977af9f
Stefan Dindyal <Stefan Dindyal>
2017-03-03 00:39:56 -0500

    update

--- patch/js/gallery-v1.js
+++ patch/js/gallery-v1.js
@@ -7,21 +7,12 @@
 
 		$('.ico.tile').on('click', function(){
 			gallery.removeClass('listed');
-			// $('.ico.reset').trigger('click');
 			app.templates.gallery.init();
-			// $('.gallery-post').each(function(){
-			// 	var it = $(this).find('.gallery-post-content');
-			// 	it.height($(this).height());
-			// });
 		});
+
 		$('.ico.list').on('click', function(){
-			gallery.addClass('listed');
-			// $('.ico.reset').trigger('click');
+			gallery.addClass('listed');			
 			app.templates.gallery.init();
-			// $('.gallery-post').each(function(){
-			// 	var it = $(this).find('.gallery-post-content');
-			// 	it.height($(this).outerHeight());
-			// });
 		});
 
 		$(window).load(function(){
@@ -37,59 +28,32 @@
 			return l;
 		};
 
-		// var frame = '<div id="campaign"><iframe width="100%" height="100%" src="" frameborder="0"></iframe></div>';
-		// $('body').append(frame);
-
 		$(document).on('click', '.info-it', function(e){	
 			var it = $(this);
 			var href = it.attr('data-perm');
 			var path = getLocation(href).pathname;
 			var state = path.split('/').pop();
-			var frame = '<div id="campaign"><iframe width="100%" height="100%" src="'+href+'" frameborder="0"></iframe></div>';
+			var frame = $('<iframe id="frameDemo" src="'+href+'" frameborder="0"/>');
 			$('body').append(frame);
-			// $('#campaign iframe').attr('src', href);
 			$('#campaign').addClass('show');
 			window.history.pushState({a: state}, '', path);		
 		});
 
-		$(document).on('click', '.demo-it', function(e){	
+		$(document).on('click', '.demo-it', function(e){
+			var href = location.href;
 			var it = $(this);
 			var code = it.attr('data-code');
 			var state = 'demo';			
-			var frame = '<div id="campaign"><iframe width="100%" height="100%" frameborder="0"></iframe></div>';
+			var frame = $('<iframe id="frameDemo" frameborder="0"/>');
 			$('body').append(frame);
-			// $('#campaign iframe').attr('src', href);			
-			var $iframe = $('#campaign iframe');
-			$iframe.ready(function() {
-				$iframe.contents().find("body").append(code);
-				// $iframe.get(0).contentWindow.location.reload(true);
-				$('#campaign').addClass('show');
-			});
-			// $('body').append('<div id="campaign">'+code+'</div>');
-			$('#campaign').addClass('show');
+			var iframe = $('#frameDemo');
+			iframe.contents().find('body').append(code);		
 			window.history.pushState({a: state}, '', '#demo');		
 		});
 
-		// $('#campaign iframe').load(function(){
-		// 	if(!$('#campaign').hasClass('show') && $('#campaign iframe').attr('src') != ''){
-		// 		$('#campaign').addClass('show');
-		// 	}			
-		// });
-
 		window.onpopstate = function(event) {
-			// console.log(event.state);
 			if(event.state.a == 'gallery'){
-				// console.log('gallery');
-				$('#campaign').remove();
-				// $('#campaign').removeClass('show');
-				// $('#campaign iframe').attr('src', '');
-				// $('#campaign').removeClass('show');
-				// $('#campaign').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', 
-				// function(){
-				// 	$('#campaign iframe').attr('src', '');
-				// });				
-			} else {
-				// console.log('campaign');
+				$('#frameDemo').remove();				
 			}			
 		}
 
