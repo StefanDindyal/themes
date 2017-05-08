53f84373ffb481d624860cfe449984cd8904245e
Stefan Dindyal <paradox880@gmail.com>
2017-05-07 23:31:44 -0400

    update

--- partials/gallery-filters.php
+++ partials/gallery-filters.php
@@ -60,23 +60,183 @@
     <div class="filter-view">
       <span class="ico reset">
         <!-- <span class="dashicons dashicons-image-rotate"></span> -->
-        <?php echo file_get_contents(get_template_directory_uri() . '/patch/images/reset.svg'); ?>
+        <?php //echo file_get_contents(get_template_directory_uri() . '/patch/images/reset.svg'); ?>
         <!-- <img src="<?php bloginfo('template_directory'); ?>/patch/images/reset.svg" alt="" border="0"> -->
+        <svg id="Reset" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 68.7 57.37"><defs><style>.cls-1{fill:#231f20;}</style></defs><title>Reset</title><path class="cls-1" d="M74.45,43.85h0V46.8h0a25.75,25.75,0,1,1-24.83,19l7.71,9.74,2.35-1.86-6.22-7.86-4.41-5.57-4.46,2.72L34.43,69.13,36,71.69l10.75-6.55A28.68,28.68,0,1,0,74.45,43.85Z" transform="translate(-34.43 -43.85)"/></svg>
       </span>
       <span class="ico tile">
         <!-- <span class="dashicons dashicons-grid-view"></span> -->
-        <?php echo file_get_contents(get_template_directory_uri() . '/patch/images/grid.svg'); ?>
+        <?php //echo file_get_contents(get_template_directory_uri() . '/patch/images/grid.svg'); ?>
         <!-- <img src="<?php bloginfo('template_directory'); ?>/patch/images/grid.svg" alt="" border="0"> -->
+        <svg id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 67.73 67.73"><defs><style>.cls-1{fill:#231f20;}</style></defs><title>Grid</title><path class="cls-1" d="M66.82,67.09H38.24V38.51H66.82Zm-25.58-3H63.82V41.51H41.24Z" transform="translate(-38.24 -38.51)"/><path class="cls-1" d="M106,67.09H77.39V38.51H106Zm-25.58-3H103V41.51H80.39Z" transform="translate(-38.24 -38.51)"/><path class="cls-1" d="M66.82,106.24H38.24V77.66H66.82Zm-25.58-3H63.82V80.66H41.24Z" transform="translate(-38.24 -38.51)"/><path class="cls-1" d="M106,106.24H77.39V77.66H106Zm-25.58-3H103V80.66H80.39Z" transform="translate(-38.24 -38.51)"/></svg>
       </span>
       <span class="ico list">
         <!-- <span class="dashicons dashicons-list-view"></span> -->
-        <?php echo file_get_contents(get_template_directory_uri() . '/patch/images/list.svg'); ?>
+        <?php //echo file_get_contents(get_template_directory_uri() . '/patch/images/list.svg'); ?>
         <!-- <img src="<?php bloginfo('template_directory'); ?>/patch/images/list.svg" alt="" border="0"> -->
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+           viewBox="0 0 144 144" style="enable-background:new 0 0 144 144;" xml:space="preserve">
+        <style type="text/css">
+          .st0{display:none;}
+          .st1{display:inline;}
+          .st2{fill:#231F20;}
+          .st3{fill:none;}
+        </style>
+        <g id="Reset" class="st0">
+          <g class="st1">
+            <path class="st2" d="M74.4,43.9C74.4,43.9,74.4,43.9,74.4,43.9l0,2.9c0,0,0,0,0,0c14.2,0,25.7,11.5,25.7,25.7S88.6,98.3,74.4,98.3
+              c-14.2,0-25.7-11.5-25.7-25.7c0-2.3,0.3-4.6,0.9-6.8l7.7,9.7l2.4-1.9l-6.2-7.9l-4.4-5.6l-4.5,2.7l-10.2,6.2l1.6,2.6l10.7-6.5
+              c-0.6,2.4-1,4.8-1,7.4c0,15.8,12.9,28.7,28.7,28.7c15.8,0,28.7-12.9,28.7-28.7S90.3,43.9,74.4,43.9z"/>
+          </g>
+        </g>
+        <g id="Layer_2" class="st0">
+          <g class="st1">
+            <g>
+              <path class="st2" d="M66.8,67.1H38.2V38.5h28.6V67.1z M41.2,64.1h22.6V41.5H41.2V64.1z"/>
+            </g>
+            <g>
+              <path class="st2" d="M106,67.1H77.4V38.5H106V67.1z M80.4,64.1H103V41.5H80.4V64.1z"/>
+            </g>
+            <g>
+              <path class="st2" d="M66.8,106.2H38.2V77.7h28.6V106.2z M41.2,103.2h22.6V80.7H41.2V103.2z"/>
+            </g>
+            <g>
+              <path class="st2" d="M106,106.2H77.4V77.7H106V106.2z M80.4,103.2H103V80.7H80.4V103.2z"/>
+            </g>
+          </g>
+        </g>
+        <g id="Layer_3">
+          <g>
+            <rect x="60.8" y="41.2" class="st2" width="44" height="3.3"/>
+            <rect x="40.1" y="41.2" class="st2" width="15.9" height="3.3"/>
+            <rect x="40.1" y="61" class="st2" width="15.9" height="3.3"/>
+            <rect x="60.8" y="61" class="st2" width="44" height="3.3"/>
+            <rect x="60.8" y="80.8" class="st2" width="44" height="3.3"/>
+            <rect x="40.1" y="80.8" class="st2" width="15.9" height="3.3"/>
+            <rect x="40.1" y="100.6" class="st2" width="15.9" height="3.3"/>
+            <rect x="60.8" y="100.6" class="st2" width="44" height="3.3"/>
+          </g>
+        </g>
+        <g id="Layer_4" class="st0">
+          <g class="st1">
+            <polygon class="st3" points="95.9,66.4 78.2,66 72,49.9 66.2,66 48.2,66.4 62.4,77.4 57,94.8 72,84.3 86.9,94.8 81.8,77.4    "/>
+            <path class="st2" d="M106.5,62.9l-25.7-0.6l-0.1-0.2L72,39.2l-8.5,23.1l-26,0.6L58,78.8l-7.7,25.3L72,88.9l21.4,15.2l-7.3-25.3
+              L106.5,62.9z M87.8,96L72,84.9L56.1,96l5.6-18.4l-15-11.6l19-0.4l6.2-17l6.5,17l18.8,0.4L82.4,77.6L87.8,96z"/>
+          </g>
+        </g>
+        <g id="Layer_5" class="st0">
+          <g class="st1">
+            <path class="st2" d="M38,56.4c6,0,11-4.5,11.7-10.4h66.7v-3H49.7C49,37.2,44,32.7,38,32.7c-6.5,0-11.9,5.3-11.9,11.9
+              S31.4,56.4,38,56.4z M29.1,44.5c0-4.9,4-8.9,8.9-8.9s8.9,4,8.9,8.9s-4,8.9-8.9,8.9S29.1,49.4,29.1,44.5z"/>
+            <path class="st2" d="M106.1,59.7c-6,0-11,4.5-11.7,10.4H27.6v3h66.8c0.7,5.8,5.7,10.3,11.7,10.3c6.5,0,11.9-5.3,11.9-11.9
+              C118,65,112.7,59.7,106.1,59.7z M97.3,71.5c0-4.9,4-8.9,8.8-8.9s8.9,4,8.9,8.9c0,4.9-4,8.9-8.9,8.9S97.3,76.4,97.3,71.5z"/>
+            <path class="st2" d="M65.3,87.7c-6,0-11,4.5-11.7,10.4h-26v3h26c0.7,5.8,5.7,10.4,11.7,10.4c6,0,11-4.5,11.7-10.4h39.3v-3H77.1
+              C76.4,92.2,71.4,87.7,65.3,87.7z M74.2,99.5c0,4.9-4,8.9-8.9,8.9c-4.9,0-8.9-4-8.9-8.9s4-8.9,8.9-8.9
+              C70.2,90.7,74.2,94.6,74.2,99.5z"/>
+          </g>
+        </g>
+        <g id="Layer_6" class="st0">
+          <g class="st1">
+            <rect x="44.8" y="37.2" class="st3" width="53.6" height="53.6"/>
+            <circle class="st3" cx="71.6" cy="104.8" r="3.2"/>
+            <circle class="st3" cx="89.6" cy="104.8" r="3.2"/>
+            <path class="st2" d="M41.8,93.8h59.6V34.2H41.8V93.8z M44.8,37.2h53.6v53.6H44.8V37.2z"/>
+            <circle class="st2" cx="53.6" cy="104.8" r="6.2"/>
+            <path class="st2" d="M71.6,98.6c-3.4,0-6.2,2.8-6.2,6.2s2.8,6.2,6.2,6.2s6.2-2.8,6.2-6.2S75,98.6,71.6,98.6z M71.6,108
+              c-1.8,0-3.2-1.4-3.2-3.2s1.4-3.2,3.2-3.2s3.2,1.4,3.2,3.2S73.4,108,71.6,108z"/>
+            <path class="st2" d="M89.6,98.6c-3.4,0-6.2,2.8-6.2,6.2s2.8,6.2,6.2,6.2s6.2-2.8,6.2-6.2S93,98.6,89.6,98.6z M89.6,108
+              c-1.8,0-3.2-1.4-3.2-3.2s1.4-3.2,3.2-3.2s3.2,1.4,3.2,3.2S91.4,108,89.6,108z"/>
+            <polygon class="st2" points="33.7,52.4 23.6,64 33.7,75.6 35.9,73.6 27.6,64 35.9,54.4    "/>
+            <polygon class="st2" points="109.5,52.4 107.2,54.4 115.6,64 107.2,73.6 109.5,75.6 119.6,64    "/>
+          </g>
+        </g>
+        <g id="Layer_7" class="st0">
+        </g>
+        </svg>
       </span>
       <span class="ico favorite">
         <!-- <span class="dashicons dashicons-star-filled"></span> -->
-        <?php echo file_get_contents(get_template_directory_uri() . '/patch/images/featured.svg'); ?>
-        <!-- <img src="<?php bloginfo('template_directory'); ?>/patch/images/featured.svg" alt="" border="0"> -->
+        <?php //echo file_get_contents(get_template_directory_uri() . '/patch/images/featured.svg'); ?>
+        <!-- <img src="<?php bloginfo('template_directory'); ?>/patch/images/featured.svg" alt="" border="0"> -->        
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+           viewBox="0 0 144 144" style="enable-background:new 0 0 144 144;" xml:space="preserve">
+        <style type="text/css">
+          .st0{display:none;}
+          .st1{display:inline;}
+          .st2{fill:#231F20;}
+          .st3{fill:none;}
+        </style>
+        <g id="Reset" class="st0">
+          <g class="st1">
+            <path class="st2" d="M74.4,43.9C74.4,43.9,74.4,43.9,74.4,43.9l0,2.9c0,0,0,0,0,0c14.2,0,25.7,11.5,25.7,25.7S88.6,98.3,74.4,98.3
+              c-14.2,0-25.7-11.5-25.7-25.7c0-2.3,0.3-4.6,0.9-6.8l7.7,9.7l2.4-1.9l-6.2-7.9l-4.4-5.6l-4.5,2.7l-10.2,6.2l1.6,2.6l10.7-6.5
+              c-0.6,2.4-1,4.8-1,7.4c0,15.8,12.9,28.7,28.7,28.7c15.8,0,28.7-12.9,28.7-28.7S90.3,43.9,74.4,43.9z"/>
+          </g>
+        </g>
+        <g id="Layer_2" class="st0">
+          <g class="st1">
+            <g>
+              <path class="st2" d="M66.8,67.1H38.2V38.5h28.6V67.1z M41.2,64.1h22.6V41.5H41.2V64.1z"/>
+            </g>
+            <g>
+              <path class="st2" d="M106,67.1H77.4V38.5H106V67.1z M80.4,64.1H103V41.5H80.4V64.1z"/>
+            </g>
+            <g>
+              <path class="st2" d="M66.8,106.2H38.2V77.7h28.6V106.2z M41.2,103.2h22.6V80.7H41.2V103.2z"/>
+            </g>
+            <g>
+              <path class="st2" d="M106,106.2H77.4V77.7H106V106.2z M80.4,103.2H103V80.7H80.4V103.2z"/>
+            </g>
+          </g>
+        </g>
+        <g id="Layer_3" class="st0">
+          <g class="st1">
+            <rect x="60.8" y="41.2" class="st2" width="44" height="3.3"/>
+            <rect x="40.1" y="41.2" class="st2" width="15.9" height="3.3"/>
+            <rect x="40.1" y="61" class="st2" width="15.9" height="3.3"/>
+            <rect x="60.8" y="61" class="st2" width="44" height="3.3"/>
+            <rect x="60.8" y="80.8" class="st2" width="44" height="3.3"/>
+            <rect x="40.1" y="80.8" class="st2" width="15.9" height="3.3"/>
+            <rect x="40.1" y="100.6" class="st2" width="15.9" height="3.3"/>
+            <rect x="60.8" y="100.6" class="st2" width="44" height="3.3"/>
+          </g>
+        </g>
+        <g id="Layer_4">
+          <g>
+            <polygon class="st3" points="95.9,66.4 78.2,66 72,49.9 66.2,66 48.2,66.4 62.4,77.4 57,94.8 72,84.3 86.9,94.8 81.8,77.4    "/>
+            <path class="st2" d="M106.5,62.9l-25.7-0.6l-0.1-0.2L72,39.2l-8.5,23.1l-26,0.6L58,78.8l-7.7,25.3L72,88.9l21.4,15.2l-7.3-25.3
+              L106.5,62.9z M87.8,96L72,84.9L56.1,96l5.6-18.4l-15-11.6l19-0.4l6.2-17l6.5,17l18.8,0.4L82.4,77.6L87.8,96z"/>
+          </g>
+        </g>
+        <g id="Layer_5" class="st0">
+          <g class="st1">
+            <path class="st2" d="M38,56.4c6,0,11-4.5,11.7-10.4h66.7v-3H49.7C49,37.2,44,32.7,38,32.7c-6.5,0-11.9,5.3-11.9,11.9
+              S31.4,56.4,38,56.4z M29.1,44.5c0-4.9,4-8.9,8.9-8.9s8.9,4,8.9,8.9s-4,8.9-8.9,8.9S29.1,49.4,29.1,44.5z"/>
+            <path class="st2" d="M106.1,59.7c-6,0-11,4.5-11.7,10.4H27.6v3h66.8c0.7,5.8,5.7,10.3,11.7,10.3c6.5,0,11.9-5.3,11.9-11.9
+              C118,65,112.7,59.7,106.1,59.7z M97.3,71.5c0-4.9,4-8.9,8.8-8.9s8.9,4,8.9,8.9c0,4.9-4,8.9-8.9,8.9S97.3,76.4,97.3,71.5z"/>
+            <path class="st2" d="M65.3,87.7c-6,0-11,4.5-11.7,10.4h-26v3h26c0.7,5.8,5.7,10.4,11.7,10.4c6,0,11-4.5,11.7-10.4h39.3v-3H77.1
+              C76.4,92.2,71.4,87.7,65.3,87.7z M74.2,99.5c0,4.9-4,8.9-8.9,8.9c-4.9,0-8.9-4-8.9-8.9s4-8.9,8.9-8.9
+              C70.2,90.7,74.2,94.6,74.2,99.5z"/>
+          </g>
+        </g>
+        <g id="Layer_6" class="st0">
+          <g class="st1">
+            <rect x="44.8" y="37.2" class="st3" width="53.6" height="53.6"/>
+            <circle class="st3" cx="71.6" cy="104.8" r="3.2"/>
+            <circle class="st3" cx="89.6" cy="104.8" r="3.2"/>
+            <path class="st2" d="M41.8,93.8h59.6V34.2H41.8V93.8z M44.8,37.2h53.6v53.6H44.8V37.2z"/>
+            <circle class="st2" cx="53.6" cy="104.8" r="6.2"/>
+            <path class="st2" d="M71.6,98.6c-3.4,0-6.2,2.8-6.2,6.2s2.8,6.2,6.2,6.2s6.2-2.8,6.2-6.2S75,98.6,71.6,98.6z M71.6,108
+              c-1.8,0-3.2-1.4-3.2-3.2s1.4-3.2,3.2-3.2s3.2,1.4,3.2,3.2S73.4,108,71.6,108z"/>
+            <path class="st2" d="M89.6,98.6c-3.4,0-6.2,2.8-6.2,6.2s2.8,6.2,6.2,6.2s6.2-2.8,6.2-6.2S93,98.6,89.6,98.6z M89.6,108
+              c-1.8,0-3.2-1.4-3.2-3.2s1.4-3.2,3.2-3.2s3.2,1.4,3.2,3.2S91.4,108,89.6,108z"/>
+            <polygon class="st2" points="33.7,52.4 23.6,64 33.7,75.6 35.9,73.6 27.6,64 35.9,54.4    "/>
+            <polygon class="st2" points="109.5,52.4 107.2,54.4 115.6,64 107.2,73.6 109.5,75.6 119.6,64    "/>
+          </g>
+        </g>
+        <g id="Layer_7" class="st0">
+        </g>
+        </svg>
       </span>      
     </div>    
     <!-- Filter View END -->
